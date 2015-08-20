db8536cde135e815a24ac1878ee88e13d72afc47
Ben Pedersen <peder541@umn.edu>
2015-08-20 11:27:01 -0500

    Update docent.js

--- js/docent.js
+++ js/docent.js
@@ -64,15 +64,6 @@ $(".video3").click(function(){
     $("#videoPlayer3").tubeplayer("play");
 });
 
-$(document).ready(function() {
-	$(window).on('resize', function(event) {
-		var $videos = $('.jquery-youtube-tubeplayer > iframe');
-		var $visibleVideo = $videos.filter(':visible');
-		$videos.height($visibleVideo.attr('height') / $visibleVideo.attr('width') * $visibleVideo.width());
-	});
-	$(window).resize();
-});
-
 // ------------------------------
 // Scroll Nav
 // http://twitter.com/mattsince87
