4cb5594f73dac6cc3d3b8b68614b624bd7bd852f
Ben Pedersen <peder541@umn.edu>
2016-02-23 00:46:02 -0600

    Update Firefox extension URL

--- js/install.js
+++ js/install.js
@@ -125,7 +125,7 @@ function installExtension() {
     else if (window.InstallTrigger) {
         var xpi = {
             DocentEDU: {
-                URL: 'https://addons.mozilla.org/firefox/downloads/file/392939/docentedu-0.13.0-fx.xpi?src=external-signup',
+                URL: 'https://addons.mozilla.org/firefox/downloads/file/402764/docentedu-0.14.0-fx.xpi?src=external-signup',
                 IconURL: 'https://docentedu.com/images/logo.png'
             }
         };
