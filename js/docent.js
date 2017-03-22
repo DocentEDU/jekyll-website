35fcd55ec674c44eb1d5551021e45b1cee79eec1
Ben Pedersen <peder541@umn.edu>
2017-03-21 20:52:33 -0500

    Update docent.js

--- js/docent.js
+++ js/docent.js
@@ -1,8 +1,7 @@
 $(window).on('message', function(event) {
-	console.log(event);
+	console.log('Message:', event);
 	event = event.originalEvent;
 	if (event.origin === 'https://insertlearning.com' && event.data == 'hideArrow') {
 		$('#arrow').hide();
 	}
 });
-console.log('hi');
