c184d6998a63694a11da40683772d648ecd8a21a
Ben Pedersen <peder541@umn.edu>
2017-03-21 20:51:18 -0500

    Update docent.js

--- js/docent.js
+++ js/docent.js
@@ -1,4 +1,5 @@
 $(window).on('message', function(event) {
+	console.log(event);
 	event = event.originalEvent;
 	if (event.origin === 'https://insertlearning.com' && event.data == 'hideArrow') {
 		$('#arrow').hide();
